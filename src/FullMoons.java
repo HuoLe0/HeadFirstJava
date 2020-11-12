7ce38ba872f03e8f8f8a7132d3fd78da55220a7b
HuoLe0 <1369269443@qq.com>
2020-11-12 17:10:37 +0800

    feat: reach page 345

--- src/FullMoons.java
+++ src/FullMoons.java
@@ -1,12 +1,10 @@
 import java.util.Calendar;
-
 import static java.lang.System.out;
 
 public class FullMoons {
     static int DAY_IM = 1000 * 60 * 60 * 24;
     public static void main(String[] args){
         Calendar c = Calendar.getInstance();
-
         c.set(2004,1,7,15,40);
         long day1 = c.getTimeInMillis();
         for (int x=0;x<60;x++){
@@ -15,5 +13,4 @@ public class FullMoons {
             out.println(String.format("full moon on %tc",c));
         }
     }
-
 }
